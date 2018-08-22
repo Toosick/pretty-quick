037e83b2897e2215d6718c8467e311b8cd122de1
Preston Tighe <preston_tighe@yahoo.com>
2018-08-22 18:26:22 -0500

    Patch in html support

--- src/isSupportedExtension.js
+++ src/isSupportedExtension.js
@@ -1,9 +1,11 @@
 import { extname } from 'path';
 import { getSupportInfo } from 'prettier';
 
-const extensions = getSupportInfo().languages.reduce(
-  (prev, language) => prev.concat(language.extensions || []),
-  []
-);
+const extensions = getSupportInfo()
+  .languages.reduce(
+    (prev, language) => prev.concat(language.extensions || []),
+    []
+  )
+  .concat('.html');
 
 export default file => extensions.includes(extname(file));
